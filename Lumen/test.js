4a6dbdf5c367f7680c0e969b02ddde6ad1c90f86
Julian Ngo <julngo@uw.edu>
2019-05-01 14:46:01 -0700

    fixed some needless code

--- Lumen/test.js
+++ Lumen/test.js
@@ -15,7 +15,6 @@ var stateVal = 0;
 var alpha = 0;
 
 const Y_AXIS = 1;
-const X_AXIS = 2;
 
 let cAlpha = 0;
 
@@ -37,7 +36,6 @@ function setup() {
 
     stars.push(new Star(x, y, r));
   }
-  //bg2.resize(windowWidth-20,windowHeight-20);
 }
 
 function draw() {
@@ -130,20 +128,11 @@ function draw() {
 function setGradient(x, y, w, h, c1, c2, axis) {
   noFill();
   if (axis === Y_AXIS) {
-    // Top to bottom gradient
     for (let i = y; i <= y + h; i++) {
       let inter = map(i, y, y + h, 0, 1);
       let c = lerpColor(c1, c2, inter-.1);
       stroke(c);
       line(x, i, x + w, i);
-    }
-  } else if (axis === X_AXIS) {
-    // Left to right gradient
-    for (let i = x; i <= x + w; i++) {
-      let inter = map(i, x, x + w, 0, 1);
-      let c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(i, y, i, y + h);
     }
   }
 }
